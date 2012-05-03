277dc334fdb5ce7692f2f403254ae49a68c80008
marcusgreen <marcusavgreen@gmail.com>
2012-05-03 22:23:18 +0100

    mainly unit tests
    Signed-off-by: marcusgreen  <marcusavgreen@gmail.com>

--- simpletest/testquestiontype.php
+++ simpletest/testquestiontype.php
@@ -1,35 +1,86 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * Unit tests for this question type.
+ * Unit tests for the shortanswer question type class.
  *
- * @copyright &copy; 2006 YOURNAME
- * @author YOUREMAILADDRESS
- * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
- * @package YOURPACKAGENAME
- *//** */
-    
-require_once(dirname(__FILE__) . '/../../../../config.php');
+ * @package    qtype
+ * @subpackage shortanswer
+ * @copyright  2007 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-global $CFG;
-require_once($CFG->libdir . '/simpletestlib.php');
-require_once($CFG->dirroot . '/question/type/QTYPEID/questiontype.php');
 
-class QTYPEID_qtype_test extends UnitTestCase {
-    var $qtype;
-    
-    function setUp() {
-        $this->qtype = new QTYPEID_qtype();
-    }
-    
-    function tearDown() {
-        $this->qtype = null;    
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/question/type/shortanswer/questiontype.php');
+
+
+/**
+ * Unit tests for the shortanswer question type class.
+ *
+ * @copyright  2007 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_gapfill_test extends UnitTestCase {
+    public static $includecoverage = array(
+        'question/type/questiontypebase.php',
+        'question/type/gapfill/questiontype.php',
+    );
+
+    protected $qtype;
+
+    public function setUp() {
+        $this->qtype = new qtype_shortanswer();
     }
 
-    function test_name() {
-        $this->assertEqual($this->qtype->name(), 'QTYPEID');
+    public function tearDown() {
+        $this->qtype = null;
     }
-    
-    // TODO write unit tests for the other methods of the question type class.
+
+    protected function get_test_question_data() {
+        $q = new stdClass();
+        $q->id = 1;
+        $q->options->answers[1] = (object) array('answer' => 'frog', 'fraction' => 1);
+        $q->options->answers[2] = (object) array('answer' => '*', 'fraction' => 0.1);
+
+        return $q;
+    }
+
+    public function test_name() {
+        $this->assertEqual($this->qtype->name(), 'shortanswer');
+    }
+
+    public function test_can_analyse_responses() {
+        $this->assertTrue($this->qtype->can_analyse_responses());
+    }
+
+    public function test_get_random_guess_score() {
+        $q = $this->get_test_question_data();
+        $this->assertEqual(0.1, $this->qtype->get_random_guess_score($q));
+    }
+
+    public function test_get_possible_responses() {
+        $q = $this->get_test_question_data();
+
+        $this->assertEqual(array(
+            $q->id => array(
+                1 => new question_possible_response('frog', 1),
+                2 => new question_possible_response('*', 0.1),
+                null => question_possible_response::no_response()),
+        ), $this->qtype->get_possible_responses($q));
+    }
 }
-
-?>
