f449c6750abea31aa82c3416f0f5d6c5ac2462f9
marcusgreen <marcusavgreen@gmail.com>
2013-04-09 17:39:58 +0100

    incremented the digit on the save point to match the one in version.php

--- db/upgrade.php
+++ db/upgrade.php
@@ -75,7 +75,7 @@ function xmldb_qtype_gapfill_upgrade($oldversion = 0) {
     }
 
     // Gapfill savepoint reached.
-    upgrade_plugin_savepoint(true, 2006082506, 'qtype', 'gapfill');
+    upgrade_plugin_savepoint(true, 2006082507, 'qtype', 'gapfill');
 
     return;
 }
