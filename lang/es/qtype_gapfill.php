6f7c667c81e64ec4c43102fbf714f2d8babd59cc
marcusgreen <marcusavgreen@gmail.com>
2013-05-28 21:38:31 +0100

    updates from Goncarlo Gomez

--- lang/es/qtype_gapfill.php
+++ lang/es/qtype_gapfill.php
@@ -25,32 +25,34 @@
  * @subpackage gapfill
  */
 
-$string['addinggapfill']='Agregando espacios para completar';
-$string['casesensitive']='entre mayúsculas y minúsculas';
-$string['casesensitive_help']='Cuando esta opción está activada, si la respuesta correcta es la del Gato, se marcará como una respuesta incorrecta ';
+$string['addinggapfill']='Agregar rellenar espacios en blanco';
+$string['casesensitive']='Mayúscula y minúscula';
+$string['casesensitive_help']='Cuando esto está seleccionado, si la respuesta correcta es “GATO”, entonces “gato” se marcará como respuesta incorrecta.';
 $string['noduplicates']='Sin duplicados';
-$string['noduplicates_help']='Cuando se activa, cada respuesta debe ser útil único, donde cada campo tiene un operador |, es decir, cuáles son los colores de las medallas olímpicas y cada campo tiene [oro | plata | bronce], sólo si el estudiante entra en oro en todos los campos de la primera recibirá una marca (los otros seguirán recibiendo una garrapata sin embargo). En realidad es más bien descartar duplicar respuestas para fines de marcado ';
+$string['noduplicates_help']='Cuando esto está seleccionado, cada respuesta debe ser única. Útil donde cada campo tenga un operador | , por ejemplo: “Cuáles son los colores de la medallas olímpicas” y cada campo tiene [oro] [plata] [bronce], si un estudiante entra oro en cada campo solamente el primero obtendrá una nota (aunque los otros también obtendrán una palomita). Es más bien para descartar las respuestas duplicadas al calificar';
 
-$string['delimitchars']='Delimitar signos o caracteres';
-$string['pluginnameediting'] = 'Editando espacios para completar';
-$string['pluginnameadding'] = 'Agregando una pregunta con espacios para completar';
+$string['delimitchars']='Delimita los caracteres ';
+$string['pluginnameediting'] = 'Editar rellenar espacio en blanco';
+$string['pluginnameadding'] = 'Agregar una pregunta de rellenar espacio en blanco';
 
-$string['gapfill'] = 'Gapfill tipo de pregunta';
+$string['gapfill'] = 'Relleno espacio tipo Cloze';
 
-$string['displaygapfill'] = 'gapfill';
-$string['displaydropdown'] = 'dropdown';
-$string['displaydragdrop'] = 'dragdrop';
+$string['displaygapfill'] = 'Rellenar espacio';
+$string['displaydropdown'] = 'desplegable';
+$string['displaydragdrop'] = 'Arrastrar/ soltar';
 
-$string['pluginname']="Pregunta de tipo con espacios para completar";
-$string['pluginname_help'] = 'Coloque las palabras para ser completado dentro de paréntesis cuadrados, por ejemplo El [cat] se sentó en el [MAT]';
+$string['pluginname']="Pregunta de rellenar espacio en blanco";
+$string['pluginname_help'] = 'Ponga las palabras que necesitan ser completadas entre corchetes; por ejemplo: El [gato] se sentó sobre [la alfombra].  Los modos desplegables y arrastrar/soltar permiten  mostrar una lista de respuestas mezcladas que también pueden incluir opcionales respuestas incorrectas o de distracción.';
 
 $string['pluginname_link']='/question/type/gapfill';
-$string['pluginnamesummary'] = 'Una pregunta del tipo complete los espacios. Menos dispositivos que el tipo Cloze estándar pero con sintaxis mas simple.';
-$string['delimitchars_help']='Cambio de los caracteres que delimitan un campo de la omisión [], útil para la programación de las cuestiones lingüísticas';
-$string['answerdisplay']='Mostrar respuestas';
-$string['answerdisplay_help'] = 'Si se marca esto, cada campo será cambiado en un desplegable que contiene todas las respuestas para todos los campos';
-$string['pleaseenterananswer']='Por favor ponga una respuesta';
-$string['wronganswers']='respuestas incorrectas.';
-$string['wronganswers_help']='Lista de palabras incorrectas separados por comas, se aplica únicamente en dragdrop / modo de menús desplegables';
+$string['pluginnamesummary'] = 'Pregunta del tipo de rellenar espacio en blanco.  Menos funciones que las clásicas de tipo Cloze, pero con una sintaxis más simple. ';
+$string['delimitchars_help']='Cambia los caracteres que delimitan un campo del predeterminado [ ], útil para las preguntas de lenguaje de programación';
+$string['questionsmissing']='Usted no se ha incluido ningún campo en el texto de la pregunta';
+
+$string['answerdisplay']='Muestra las respuestas';
+$string['answerdisplay_help'] = 'Si está seleccionado, esto convertirá cada campo en un menú de desplegable que contiene todas las respuestas para cada campo.';
+$string['pleaseenterananswer']='Por favor introduzca una respuesta';
+$string['wronganswers']='Respuestas incorrectas.';
+$string['wronganswers_help']='Lista de la palabras incorrectas separadas por comas, solamente es relevante en los  modos de arrastrar/soltar y desplegables';
 
 
