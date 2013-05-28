0338b7a00eed2c539cc69b7a47bd953b86eeb7eb
marcusgreen <marcusavgreen@gmail.com>
2013-05-28 21:29:00 +0100

    formatting update to fit standards

--- backup/moodle2/backup_qtype_gapfill_plugin.class.php
+++ backup/moodle2/backup_qtype_gapfill_plugin.class.php
@@ -55,7 +55,7 @@ class backup_qtype_gapfill_plugin extends backup_qtype_plugin {
 
         // Now create the qtype own structures.
         $gapfill = new backup_nested_element('gapfill', array('id'), array(
-            'answerdisplay', 'delimitchars', 'casesensitive','noduplicates',
+            'answerdisplay', 'delimitchars', 'casesensitive', 'noduplicates',
             'correctfedback', 'correctfeddbackformat', 'partiallycorrectfeedback',
             'partiallycorrectfeedbackformat',
             'incorrectfeedback', 'incorrectfeedbackformat'));
