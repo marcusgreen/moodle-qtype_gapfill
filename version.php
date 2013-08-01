f7b88c3a3b796b04aa6eb9b79d6da31516b287d4
marcusgreen <marcusavgreen@gmail.com>
2013-08-01 16:09:44 +0100

    incremented version value

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_gapfill';
-$plugin->version  = 2006082508;
+$plugin->version  = 2006082509;
 $plugin->requires = 2012120305;
 $plugin->release ='1.2';
 $plugin->maturity  = MATURITY_STABLE;
