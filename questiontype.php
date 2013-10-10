44bd9d3911111cc1d9dc9e48ce7eb7b97c80da69
marcusgreen <marcusavgreen@gmail.com>
2013-10-10 19:00:17 +0100

    changed back to a simple regex that works (doesn't escape commas though)

--- questiontype.php
+++ questiontype.php
@@ -254,10 +254,10 @@ class qtype_gapfill extends question_type {
                 /* remove any trailing commas */
                 $question->wronganswers = rtrim($question->wronganswers, ',');
                 /* this allows escaped commas in wrong answers, i.e. \, */
-               $regex = '/(.*?[^\\\\](\\\\\\\\)*?),/';
-               preg_match_all($regex, $question->wronganswers.',', $wronganswers);
-                $wronganswers = $wronganswers[0];
-                $wronganswerfields=array();
+             //  $regex = '/(.*?[^\\\\](\\\\\\\\)*?),/';
+                $regex='/,/';
+             $wronganswers = preg_split($regex, $question->wronganswers);
+             $wronganswerfields=array();
                 foreach ($wronganswers as $key => $word) {
                     $wronganswerfields[$key]['value'] = $word;
                     $wronganswerfields[$key]['fraction'] = 0;
