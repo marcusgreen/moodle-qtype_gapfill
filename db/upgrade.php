944e104bb0c8dc73923b76af99b1fc33eeae777d
Eduardo Hernandez Montesinos <ehernandez@serviclients.com>
2014-05-27 13:03:09 +0100

    Update upgrade.php
    The problem is that when building the SQL at the end of a line put a carriage return and continued on another line by the end of the SQL statement. 
    
    Regardless of the database (mysql or postgres or oracle ...) is used, it is best to put that construction of SQL statements on a single line, without adding a carriage return.

--- db/upgrade.php
+++ db/upgrade.php
@@ -35,13 +35,11 @@ function xmldb_qtype_gapfill_upgrade($oldversion = 0) {
     if ($oldversion < 2006082505) {
 
         /* some fractions may be zero which will confuse the new way of marking */
-        $sql = "Update " . $CFG->prefix . "question_answers qa," . $CFG->prefix . "question q set
-        qa.fraction='1' where q.id=qa.question and q.qtype='gapfill'";
+        $sql = "update " . $CFG->prefix . "question_answers qa," . $CFG->prefix . "question q set qa.fraction='1' where q.id=qa.question and q.qtype='gapfill'";
 
         $DB->execute($sql);
 
-        $rs = $DB->get_recordset_sql("SELECT wronganswers, question
-                                        FROM {question_gapfill}");
+        $rs = $DB->get_recordset_sql("SELECT wronganswers, question FROM {question_gapfill}");
 
         foreach ($rs as $gf) {
             if ($gf->wronganswers == '') {
@@ -61,16 +59,13 @@ function xmldb_qtype_gapfill_upgrade($oldversion = 0) {
         $DB->change_database_structure("ALTER TABLE " . $CFG->prefix . "question_gapfill drop column wronganswers");
         $DB->change_database_structure("ALTER TABLE " . $CFG->prefix . "question_gapfill drop column shuffledanswers");
 
-        $sql = "ALTER TABLE " . $CFG->prefix . "question_gapfill add column noduplicates tinyint(1)
-            default 1 after casesensitive   ";
+        $sql = "ALTER TABLE " . $CFG->prefix . "question_gapfill add column noduplicates tinyint(1) default 1 after casesensitive";
         $DB->change_database_structure($sql);
-        $DB->change_database_structure("ALTER TABLE " . $CFG->prefix . "question_gapfill add column
-            'noduplicates' int(1) default 1 NULL ");
+        $DB->change_database_structure("ALTER TABLE " . $CFG->prefix . "question_gapfill add column 'noduplicates' int(1) default 1 NULL ");
         $rs->close();
     }
     if ($oldversion == 2006082507) {
-        $sql = "ALTER TABLE " . $CFG->prefix . "question_gapfill add column noduplicates tinyint(1) default 1 af
-            ter casesensitive   ";
+        $sql = "ALTER TABLE " . $CFG->prefix . "question_gapfill add column noduplicates tinyint(1) default 1 after casesensitive   ";
         $DB->change_database_structure($sql);
     }
     if ($oldversion < 2006082510) {
