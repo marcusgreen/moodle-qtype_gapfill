384e6cdfe47a66d72bf5dd45f5208b9dc4f1c5db
marcusavgreen@gmail.com <marcusavgreen@gmail.com>
2015-01-22 22:42:20 +0000

    codechecker formatting

--- lang/en/qtype_gapfill.php
+++ lang/en/qtype_gapfill.php
@@ -25,13 +25,13 @@
  * @subpackage gapfill
  */
 
-$string['casesensitive']='Case Sensitive';
-$string['casesensitive_help']='When this is checked, if the correct answer is CAT, cat will be flagged as a wrong answer';
-$string['noduplicates']='No Duplicates';
-$string['noduplicates_help']='When checked, each answer must be unique, useful where each field has a | operator, i.e. what are the colours of the Olympic medals and each field has [gold|silver|bronze], if the student enters gold in every field only the first will get a mark (the others will still get a tick though). It is really more like discard duplicate answers for marking purposes';
+$string['casesensitive'] = 'Case Sensitive';
+$string['casesensitive_help'] = 'When this is checked, if the correct answer is CAT, cat will be flagged as a wrong answer';
+$string['noduplicates'] = 'No Duplicates';
+$string['noduplicates_help']= 'When checked, each answer must be unique, useful where each field has a | operator, i.e. what are the colours of the Olympic medals and each field has [gold|silver|bronze], if the student enters gold in every field only the first will get a mark (the others will still get a tick though). It is really more like discard duplicate answers for marking purposes';
 
 
-$string['delimitchars']='Delimit characters';
+$string['delimitchars'] = 'Delimit characters';
 $string['pluginnameediting'] = 'Editing Gap Fill.';
 $string['pluginnameadding'] = 'Adding a Gap Fill Question.';
 
@@ -41,26 +41,26 @@ $string['displaygapfill'] = 'gapfill';
 $string['displaydropdown'] = 'dropdown';
 $string['displaydragdrop'] = 'dragdrop';
 
-$string['pluginname']="Gapfill";
+$string['pluginname'] = 'Gapfill';
 $string['pluginname_help'] = 'Place the words to be completed within square brackets e.g. The [cat] sat on the [mat].  If mat or rug are acceptable use [mat|rug]. Dropdown and Dragdrop modes allows for a shuffled list of answers to be displayed which can include optional wrong/distractor answers.';
 
-$string['pluginname_link']='question/type/gapfill';
+$string['pluginname_link'] = 'question/type/gapfill';
 $string['pluginnamesummary'] = 'A fill in the gaps style question. Fewer features than the standard Cloze type, but simpler syntax';
-$string['questionsmissing']='You have not included any fields in your question text';
-$string['delimitchars_help']='Change the characters that delimit a field from the default [ ], useful for programming language questions';
-$string['answerdisplay']='Display Answers';
+$string['questionsmissing'] = 'You have not included any fields in your question text';
+$string['delimitchars_help'] = 'Change the characters that delimit a field from the default [ ], useful for programming language questions';
+$string['answerdisplay'] = 'Display Answers';
 $string['answerdisplay_help'] = 'Dragdrop shows a list of words that can be dragged into the gaps, gapfill shows gaps with no word options, dropdown shows the same list of correct (and possibly incorrect) answers for each field';
-$string['pleaseenterananswer']='Please enter an answer.';
+$string['pleaseenterananswer'] = 'Please enter an answer.';
 $string['duplicatepartialcredit'] = 'Credit is partial because you have duplicate answers';
 $string['disableregex'] = 'Disable Regex';
 $string['disableregex_help'] = 'Disable regular expression processing and perform a standard string comparison. This can be useful for html quesitons where the angle brackets (&lt; and &gt;) should be treated literally and maths where symbols such as * should be seen literally rather than as expressions';
-$string['disableregexset_text']='Disable regular expression processing of responses';
-$string['fixedgapsize']='Fixed Gap Size';
+$string['disableregexset_text'] = 'Disable regular expression processing of responses';
+$string['fixedgapsize'] = 'Fixed Gap Size';
 $string['fixedgapsize_help'] = 'When attempting the question all gaps will be set to the same size as the largest gap. This removes gap size as a clue to the correct answer, e.g. if the gaps are [red] and [yellow] it would be clear that the yellow went in the biggest gap';
 $string['fixedgapsizeset_text']='Sets the size of every gap to that of the biggest gap';
-$string['delimitset']='Delimit Chars';
-$string['moreoptions']='More Options.';
-$string['orblank']='or blank ';
+$string['delimitset'] = 'Delimit Chars';
+$string['moreoptions'] = 'More Options.';
+$string['orblank'] = 'or blank ';
 $string['or']=' or ';
 $string['delimitset_text']='Sets the delimiters for gaps, so you could add % % for The %cat% sat on the %mat%';
 $string['wronganswers']='Distractors.';
