47614cafe87e84420fa60db48c77d41150dccff4
marcusavgreen@gmail.com <marcusavgreen@gmail.com>
2015-01-24 21:38:57 +0000

    codechecker formatting

--- settings.php
+++ settings.php
@@ -30,7 +30,7 @@ if ($ADMIN->fulltree) {
         get_string('fixedgapsize', 'qtype_gapfill'),
         get_string('fixedgapsizeset_text', 'qtype_gapfill') , 0));
     $settings->add(new admin_setting_configtextarea('qtype_gapfill/delimitchars',
-         get_string('delimitset','qtype_gapfill'),
-         get_string('delimitset_text','qtype_gapfill'),
+         get_string('delimitset', 'qtype_gapfill'),
+         get_string('delimitset_text', 'qtype_gapfill'),
          "[ ],{ },# #,@ @", PARAM_RAW, 20, 3));
 }
