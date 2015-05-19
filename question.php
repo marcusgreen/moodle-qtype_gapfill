8db9f67a73307aaeae3acea796c8b33b4c0a7015
marcusavgreen@gmail.com <marcusavgreen@gmail.com>
2015-05-19 21:13:38 +0100

    formatting

--- question.php
+++ question.php
@@ -190,7 +190,8 @@ class qtype_gapfill_question extends question_graded_automatically_with_countbac
         }
     }
 
-     /* A question is gradable if at least one gap response is not blank */
+    /* A question is gradable if at least one gap response is not blank */
+
     public function is_gradable_response(array $response) {
         foreach ($response as $key => $answergiven) {
             if (($answergiven !== "")) {
@@ -216,12 +217,11 @@ class qtype_gapfill_question extends question_graded_automatically_with_countbac
     /* called from within renderer in interactive mode */
 
     public function is_correct_response($answergiven, $rightanswer) {
-      
         if (!$this->casesensitive == 1) {
-            $answergiven = mb_strtolower ($answergiven);
-            $rightanswer = mb_strtolower ($rightanswer);
+            $answergiven = mb_strtolower($answergiven);
+            $rightanswer = mb_strtolower($rightanswer);
         }
-  
+
         if ($this->compare_response_with_answer($answergiven, $rightanswer, $this->disableregex)) {
             return true;
         } else if (($answergiven == "") && (preg_match($this->blankregex, $rightanswer))) {
@@ -248,7 +248,7 @@ class qtype_gapfill_question extends question_graded_automatically_with_countbac
                 continue;
             }
             if (!$this->casesensitive == 1) {
-                $answergiven =mb_strtolower($answergiven);
+                $answergiven = mb_strtolower($answergiven);
                 $rightanswer = mb_strtolower($rightanswer);
             }
             if ($this->compare_response_with_answer($answergiven, $rightanswer, $this->disableregex)) {
@@ -353,12 +353,12 @@ class qtype_gapfill_question extends question_graded_automatically_with_countbac
         }
     }
 
-   public function compare_response_with_answer($answergiven, $answer, $disableregex = false) {
-     
+    public function compare_response_with_answer($answergiven, $answer, $disableregex = false) {
+
         /* converts things like &lt; into < */
         $answer = htmlspecialchars_decode($answer);
         $answergiven = htmlspecialchars_decode($answergiven);
-   
+
         /* useful with questions containing html code or math symbols */
         if ($disableregex == true) {
             /* strcmp is case sensitive. If case sensitive is off both string and
@@ -373,19 +373,18 @@ class qtype_gapfill_question extends question_graded_automatically_with_countbac
                 return false;
             }
         }
-               
+
         $pattern = str_replace('/', '\/', $answer);
-        $regexp="";
+        $regexp = "";
         /* if the gap contains | then only match complete words
          * this is to avoid a situation where [cat|dog]
          * would match catty or bigcat and adog and doggy
          */
-        if(strpos($pattern,"|")){
+        if (strpos($pattern, "|")) {
             $regexp = '/\b(' . $pattern . ')\b/u';
-        }else{
-             $regexp = '/^' . $pattern . '$/u';
+        } else {
+            $regexp = '/^' . $pattern . '$/u';
         }
-      
 
         // Make the match insensitive if requested to, not sure this is necessary.
         if (!$this->casesensitive) {
