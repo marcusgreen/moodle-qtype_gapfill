be3ab423d45a93c75944f7d9ec1d262525c9eb40
marcusavgreen@gmail.com <marcusavgreen@gmail.com>
2015-05-26 22:48:04 +0100

    updated link to new version of jquery-ui

--- questiontype.php
+++ questiontype.php
@@ -97,7 +97,7 @@ class qtype_gapfill extends question_type {
             // Moodle <= 2.6.
             $jquery = '/question/type/' . $this->name().'/jquery';
             $PAGE->requires->js($jquery.'/jquery-1.9.1.min.js', true);
-            $PAGE->requires->js($jquery.'/jquery-ui-1.10.3.custom.min.js', true);
+            $PAGE->requires->js($jquery.'/jquery-ui-1.11.4.min.js', true);
             $PAGE->requires->js($jquery.'/jquery-ui.touch-punch.js', true);
         }
     }
