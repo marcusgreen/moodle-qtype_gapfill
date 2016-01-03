8c5f1feec3a2a74c535aa25f3b66f4c1e97f4830
unknown <marcusavgreen@gmail.com>
2016-01-03 09:35:52 +0000

    incremented requires to match 2.5 (previously 2.5). Really needs 2.6

--- version.php
+++ version.php
@@ -18,6 +18,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_gapfill';
 $plugin->version  = 2006082517;
-$plugin->requires = 2012120305;
+$plugin->requires = 2013051409;
 $plugin->release = '1.9';
 $plugin->maturity  = MATURITY_STABLE;
