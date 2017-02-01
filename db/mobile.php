e2e5461613e986bb8179d1535c4a328bb0ef261b
Marcus Green <marcusavgreen@gmail.com>
2017-02-01 21:51:15 +0000

    added define MOODLE

--- db/mobile.php
+++ db/mobile.php
@@ -21,6 +21,7 @@
  * @copyright  2016 Marcus Green
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+defined('MOODLE_INTERNAL') || die();
 
 $addons = array(
     "qtype_wordselect" => array()
