a4d8b95af974dec3988ac10093f0d25c32602a96
Marcus Green <marcusavgreen@gmail.com>
2017-09-15 18:39:33 +0100

    jshint updates

--- questionedit.js
+++ questionedit.js
@@ -36,7 +36,7 @@ if (settingsdata > "") {
         settings.push(obj[o]);
     }
 }
-  
+
 function Item(text, delimitchars) {
     this.questionid = $("input[name=id]").val();
     this.text = text;
@@ -50,55 +50,55 @@ function Item(text, delimitchars) {
     this.endchar = this.text.substring(this.len - 1, this.len);
     this.text_nodelim = '';
     this.feedback = {};
-    this.instance=0;
+    this.instance = 0;
     this.feedback.correct = $("#id_corecteditable").html(),
-    this.feedback.incorrect = $("#id_incorrecteditable").html();
-    Item.prototype.striptags = function(text){
+            this.feedback.incorrect = $("#id_incorrecteditable").html();
+    Item.prototype.striptags = function (text) {
         /*this is not a perfect way of stripping html but it may be good enough */
-        if(text === undefined){ 
+        if (text === undefined) {
             return "";
         }
         var regex = /(<([^>]+)>)/ig;
         return text.replace(regex, "");
-    }
+    };
     this.stripdelim = function () {
-                if (this.startchar === this.l) {
-                    this.text_nodelim = this.text.substring(1, this.len);
-                }
-                if (this.endchar === this.r) {
-                    len = this.text_nodelim.length;
-                    this.text_nodelim = this.text_nodelim.substring(0, len - 1);
-                }
-                return this.text_nodelim;
-            }
-    itemsettings = new Array();
+        if (this.startchar === this.l) {
+            this.text_nodelim = this.text.substring(1, this.len);
+        }
+        if (this.endchar === this.r) {
+            len = this.text_nodelim.length;
+            this.text_nodelim = this.text_nodelim.substring(0, len - 1);
+        }
+        return this.text_nodelim;
+    };
+    var itemsettings = [];
     Item.prototype.get_itemsettings = function (target) {
         var itemid = target.id;
-        var underscore=itemid.indexOf("_");
-        var id = itemid.substr(2,underscore);
-        id = id.substr(0,id.indexOf("_"));
+        var underscore = itemid.indexOf("_");
+        var id = itemid.substr(2, underscore);
+        id = id.substr(0, id.indexOf("_"));
         /*The instance, normally 0 but incremented if a gap has the ame text as another
          * instance is not currently used*/
-        this.instance=itemid.substr(underscore+1);
+        this.instance = itemid.substr(underscore + 1);
         for (var set in settings) {
             text = this.stripdelim();
             if (settings[set].text === text) {
-                    itemsettings = settings[set];
+                itemsettings = settings[set];
             }
         }
         return itemsettings;
     };
     this.update_json = function (e) {
-        found = false;
+        var found = false;
         var id = e.target.id;
         for (var set in settings) {
-            if (settings[set].text === this.stripdelim()){
-                    settings[set].correctfeedback = $("#id_correcteditable")[0].innerHTML;
-                    settings[set].incorrectfeedback = $("#id_incorrecteditable")[0].innerHTML;
-                    found = true;
-                }
+            if (settings[set].text === this.stripdelim()) {
+                settings[set].correctfeedback = $("#id_correcteditable")[0].innerHTML;
+                settings[set].incorrectfeedback = $("#id_incorrecteditable")[0].innerHTML;
+                found = true;
+            }
         }
-        if(found === false) {
+        if (found === false) {
             /* if there is no record for this word add one */
             var itemsettings = {
                 itemid: id,
@@ -127,13 +127,13 @@ $("#id_itemsettings_button").on("click", function () {
         $("#id_error_itemsettings")[0].innerHTML = M.util.get_string("itemsettingserror", "qtype_gapfill");
         return;
     }
-    $(".atto_html_button").attr("disabled",'true');
+    $(".atto_html_button").attr("disabled", 'true');
     if ($('#id_questiontexteditable').get(0).isContentEditable) {
         $("#id_questiontexteditable").attr('contenteditable', 'false');
         $("#fitem_id_questiontext").find('button').attr("disabled", 'true');
         var settingformheight = $("#id_questiontexteditable").css("height");
         var settingformwidth = $("#id_questiontexteditable").css("width");
-        $("#id_questiontexteditable").css("display", 'none');           
+        $("#id_questiontexteditable").css("display", 'none');
         $('#id_itemsettings_canvas').copyCSS("#id_questiontexteditable");
         var ed = $("#id_questiontexteditable").closest(".editor_atto_content_wrap");
         $("#id_itemsettings_canvas").appendTo(ed).css("position", "relative");
@@ -149,9 +149,9 @@ $("#id_itemsettings_button").on("click", function () {
         $("#id_itemsettings_canvas").css({height: "100%", width: "100%"});
         $("#id_itemsettings_button").html(M.util.get_string("editquestiontext", "qtype_gapfill"));
         /* disable the buttons on questiontext but not on the feedback form */
-       // $('#questiontext [class^=atto_]').attr("disabled",'true');
+        // $('#questiontext [class^=atto_]').attr("disabled",'true');
         /*wrapContent should be the last on this block as it sometimes falls over with an error */
-         wrapContent($("#id_itemsettings_canvas")[0]);
+        wrapContent($("#id_itemsettings_canvas")[0]);
 
     } else {
         $("#id_questiontexteditable").css({display: "block", backgroundColor: "white"});
@@ -159,7 +159,7 @@ $("#id_itemsettings_button").on("click", function () {
         $("#id_itemsettings_canvas").css("display", "none");
         $("#fitem_id_questiontext").find('button').removeAttr("disabled");
         $("#id_settings_popup").css("display", "none");
-        $("#id_itemsettings_button").html( M.util.get_string("additemsettings", "qtype_gapfill"));
+        $("#id_itemsettings_button").html(M.util.get_string("additemsettings", "qtype_gapfill"));
         $('[class^=atto_]').removeAttr("disabled");
 
     }
@@ -172,47 +172,47 @@ $("#id_itemsettings_canvas").on("click", function (e) {
      * with id followed by one or more digits and an underscore 
      * */
     if (!$('#id_questiontexteditable').get(0).isContentEditable && (e.target.id.match(/^id[0-9]+_/))) {
-        delimitchars = $("#id_delimitchars").val();
-        var item = new Item(e.target.innerHTML,delimitchars);
-            itemsettings = item.get_itemsettings(e.target);
-            if (itemsettings === null || itemsettings.length === 0) {
-                $("#id_correcteditable").html('');
-                $("#id_incorrecteditable").html('');
-            } else {
-                $("#id_correcteditable").html(itemsettings.correctfeedback);
-                $("#id_incorrecteditable").html(itemsettings.incorrectfeedback);
-            }
-            $("label[for*='id_correct']").text(M.util.get_string("correct", "qtype_gapfill"));
-            $("label[for*='id_incorrect']").text(M.util.get_string("incorrect", "qtype_gapfill"));
-            var title = M.util.get_string("additemsettings", "qtype_gapfill");
-            /* the html jquery call will turn any encoded entities such as &gt; to html, i.e. > */
-            title += ': ' + $("<div/>").html(item.stripdelim()).text();
-            var $popup = $("#id_itemsettings_popup");
-            $popup.dialog({
-                position: {
-                    my: 'right',
-                    at: 'right',
-                    of: "#id_itemsettings_canvas"
-                },
-                height: 500,
-                width: "70%",
-                modal: false,
-                title: title,
-                buttons: [
-                    {
-                        text: "OK",
-                        click: function () {
-                            var JSONstr = item.update_json(e);
-                            $("[name='itemsettingsdata']").val(JSONstr);
-                            $(this).dialog("close");
-                            /*set editable to true as it is checked at the start of click */
-                            $("#id_questiontexteditable").attr('contenteditable', 'true');
-                            $("#id_itemsettings_button").click();
-                        }
+        var delimitchars = $("#id_delimitchars").val();
+        var item = new Item(e.target.innerHTML, delimitchars);
+        itemsettings = item.get_itemsettings(e.target);
+        if (itemsettings === null || itemsettings.length === 0) {
+            $("#id_correcteditable").html('');
+            $("#id_incorrecteditable").html('');
+        } else {
+            $("#id_correcteditable").html(itemsettings.correctfeedback);
+            $("#id_incorrecteditable").html(itemsettings.incorrectfeedback);
+        }
+        $("label[for*='id_correct']").text(M.util.get_string("correct", "qtype_gapfill"));
+        $("label[for*='id_incorrect']").text(M.util.get_string("incorrect", "qtype_gapfill"));
+        var title = M.util.get_string("additemsettings", "qtype_gapfill");
+        /* the html jquery call will turn any encoded entities such as &gt; to html, i.e. > */
+        title += ': ' + $("<div/>").html(item.stripdelim()).text();
+        var $popup = $("#id_itemsettings_popup");
+        $popup.dialog({
+            position: {
+                my: 'right',
+                at: 'right',
+                of: "#id_itemsettings_canvas"
+            },
+            height: 500,
+            width: "70%",
+            modal: false,
+            title: title,
+            buttons: [
+                {
+                    text: "OK",
+                    click: function () {
+                        var JSONstr = item.update_json(e);
+                        $("[name='itemsettingsdata']").val(JSONstr);
+                        $(this).dialog("close");
+                        /*set editable to true as it is checked at the start of click */
+                        $("#id_questiontexteditable").attr('contenteditable', 'true');
+                        $("#id_itemsettings_button").click();
                     }
-                ]
-            });
-     }
+                }
+            ]
+        });
+    }
 });
 
 
@@ -241,9 +241,9 @@ var wrapContent = (function () {
         }
         var frag, text;
         var delimitchars = $("#id_delimitchars").val();
-        var l=delimitchars.substring(0,1);
-        var r=delimitchars.substring(1,2);
-        var regex = new RegExp("(\\"+l+".*?\\"+r+")","g");
+        var l = delimitchars.substring(0, 1);
+        var r = delimitchars.substring(1, 2);
+        var regex = new RegExp("(\\" + l + ".*?\\" + r + ")", "g");
         var sp, span = document.createElement('span');
         // Tag names of elements to skip, there are more to add
         var skip = {'script': '', 'button': '', 'input': '', 'select': '',
@@ -253,11 +253,11 @@ var wrapContent = (function () {
             node = nodes[i];
             // If it's an element, call wrapContent
             if (node.nodeType === 1 && !(node.tagName.toLowerCase() in skip)) {
-                wrapContent(node);                
+                wrapContent(node);
                 // If it's a text node, wrap words
             } else if (node.nodeType === 3) {
-                var textsplit = new RegExp("(\\"+l+".*?\\"+r+")","g");
-                text = node.data.split(textsplit); 
+                var textsplit = new RegExp("(\\" + l + ".*?\\" + r + ")", "g");
+                text = node.data.split(textsplit);
                 if (text) {
                     // Create a fragment, handy suckers these
                     frag = document.createDocumentFragment();
@@ -267,21 +267,21 @@ var wrapContent = (function () {
                             sp = span.cloneNode(false);
                             count++;
                             sp.className = 'item';
-                            var item = new Item(text[j],$("#id_delimitchars").val()); 
+                            var item = new Item(text[j], $("#id_delimitchars").val());
                             if (item.text > '') {
-                                var instance=0;
-                                for(var i=0; i < gaps.length; ++i){
-                                        if(gaps[i] === item.text){
-                                            instance++;
-                                        }
+                                var instance = 0;
+                                for (var i = 0; i < gaps.length; ++i) {
+                                    if (gaps[i] === item.text) {
+                                        instance++;
+                                    }
                                 }
-                                item.id='id'+count +'_'+ instance;
-                                sp.id=item.id;
-                                var is = item.get_itemsettings(item);  
-                                if (((item.striptags(is.correctfeedback) > "") || (item.striptags(is.incorrectfeedback)> ""))) {
-                                        sp.className = 'hasfeedback';
-                                 }
-                                 gaps.push(item.text);
+                                item.id = 'id' + count + '_' + instance;
+                                sp.id = item.id;
+                                var is = item.get_itemsettings(item);
+                                if (((item.striptags(is.correctfeedback) > "") || (item.striptags(is.incorrectfeedback) > ""))) {
+                                    sp.className = 'hasfeedback';
+                                }
+                                gaps.push(item.text);
                             }
                             sp.appendChild(document.createTextNode(text[j]));
                             frag.appendChild(sp);
@@ -294,8 +294,8 @@ var wrapContent = (function () {
                 }
                 // Replace the original node with the fragment
                 //if(node.parentNode !==null){
-                    node.parentNode.replaceChild(frag, node);
-              //}
+                node.parentNode.replaceChild(frag, node);
+                //}
             }
         }
     };
