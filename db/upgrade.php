f5689741be19dc1ab863296907859a81b56b063d
Marcus Green <marcusavgreen@gmail.com>
2017-12-24 08:07:56 +0000

    added gapfill_feedback table to upgrade.php

--- db/upgrade.php
+++ db/upgrade.php
@@ -80,16 +80,27 @@ function xmldb_qtype_gapfill_upgrade($oldversion = 0) {
         $table = new xmldb_table('question_gapfill');
         $dbman->add_field($table, $field);
     }
-    if (!$dbman->table_exists('question_gapfill_settings')) {
+  if (!$dbman->table_exists('question_gapfill_settings')) {
         $table = new xmldb_table('question_gapfill_settings');
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '1', null, null, true, null);
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('question', XMLDB_TYPE_CHAR, '10', null, true, null, null);
         $table->add_field('itemid', XMLDB_TYPE_CHAR, '10', null, true, null, null);
         $table->add_field('gaptext', XMLDB_TYPE_CHAR, '255', null, true, null, null);
-        $table->add_field('correctfeedback', XMLDB_TYPE_TEXT, '350', null, true, null, null);
-        $table->add_field('incorrectfeedback', XMLDB_TYPE_TEXT, '350', null, true, null, null);
+        $table->add_field('correctfeedback', XMLDB_TYPE_TEXT, '512', null, true, null, null);
+        $table->add_field('incorrectfeedback', XMLDB_TYPE_TEXT, '512', null, true, null, null);
         $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-
+        $dbman->create_table($table);
+    }
+    
+    if (!$dbman->table_exists('question_gapfill_feedback')) {
+        $table = new xmldb_table('question_gapfill_settings');
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('question', XMLDB_TYPE_CHAR, '10', null, true, null, null);
+        $table->add_field('itemid', XMLDB_TYPE_CHAR, '10', null, true, null, null);
+        $table->add_field('gaptext', XMLDB_TYPE_CHAR, '255', null, true, null, null);
+        $table->add_field('response', XMLDB_TYPE_TEXT, '512', null, true, null, null);
+        $table->add_field('feedback', XMLDB_TYPE_TEXT, '512', null, true, null, null);
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
         $dbman->create_table($table);
     }
     // Gapfill savepoint reached.
