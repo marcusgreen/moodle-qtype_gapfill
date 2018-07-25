365e17da3068e411fbd0e4f57bb47e93e5373bc4
marcusgreen <marcusavgreen@gmail.com>
2018-07-25 19:29:12 +0100

    remove mention of certificate

--- db/mobile.php
+++ db/mobile.php
@@ -1,5 +1,4 @@
 <?php
-// This file is part of the Certificate module for Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,7 +14,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Certificate module capability definition
+ * Gapfill question type  capability definition
  *
  * @package    qtype_gapfill
  * @copyright  2016 Marcus Green
