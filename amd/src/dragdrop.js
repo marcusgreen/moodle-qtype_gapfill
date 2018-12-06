86793097e502c985ebf4620e44ed01d666bce3bc
marcusgreen <marcusavgreen@gmail.com>
2018-12-06 13:11:56 +0000

    changed jshint to eslint for disabling no-unused-vars

--- amd/src/dragdrop.js
+++ amd/src/dragdrop.js
@@ -23,7 +23,7 @@
  */
 /* This should be called script.js and go through the Moodle minify process but that seems to break it */
 /* global $ */
-/*jshint unused:true */
+/* eslint disable no-unused-vars */
 define(['jquery', 'jqueryui', 'qtype_gapfill/jquery.ui.touch-punch-improved'], function($) {
     return {
         init: function() {
