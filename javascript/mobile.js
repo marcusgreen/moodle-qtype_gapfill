2b55dddfbe700a7964be6356027a574f75af25ce
marcusgreen <marcusavgreen@gmail.com>
2018-12-23 21:44:29 +0000

    readonly set to readOnly to match core question types

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -98,7 +98,7 @@ var result = {
         const answeroptions = div.querySelector('.answeroptions');
 
         if (div.querySelector('.readonly') !== null) {
-            this.question.readonly = true;
+            this.question.readOnly = true;
         }
         if (div.querySelector('.feedback') !== null) {
             this.question.feedback = div.querySelector('.feedback');
