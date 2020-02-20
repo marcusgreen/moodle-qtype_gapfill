247b3e8263e6fe1396430ac0991b240da9957735
marcusgreen <marcusgreen@mailbox.org>
2020-02-20 21:41:06 +0000

    formatting

--- question.php
+++ question.php
@@ -204,7 +204,7 @@ class qtype_gapfill_question extends question_graded_automatically_with_countbac
             }
         }
         $this->allanswers = $temp;
-        if ($this->noshuffle !=="1") {
+        if ($this->noshuffle !== "1") {
            shuffle($this->allanswers);
         }
         $step->set_qt_var('_allanswers', serialize($this->allanswers));
