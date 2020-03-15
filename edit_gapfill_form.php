c480d19b35ef637cf577668c44bf6e7f7608798f
marcusgreen <marcusgreen@mailbox.org>
2020-03-15 16:51:15 +0000

    phpcbf on all php files

--- edit_gapfill_form.php
+++ edit_gapfill_form.php
@@ -13,7 +13,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  * The editing form code for this question type.
  * @package    qtype_gapfill
