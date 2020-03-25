d3b0c90b7ecff127592b627c0e6120aeb03e6e24
marcusgreen <marcusgreen@mailbox.org>
2020-03-25 07:18:35 +0000

    removed spurious use of use (it was a test)

--- renderer.php
+++ renderer.php
@@ -22,7 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
-use qtype_gapfill_question;
 
 /**
  * Generates the output for gapfill questions
