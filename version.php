527c0ef45fc26611c2f73d277d59a339c320d71b
Marcus Green <marcusgreen@marcuss-mbp-2.home>
2021-06-30 11:40:29 +0100

    fix es6 in mobile.js for eslint error, update changelog, bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_gapfill';
-$plugin->version  = 2021051500;
+$plugin->version  = 2021063000;
 $plugin->requires = 2017042100;  // Moodle 3.3 .
-$plugin->release = '2.12';
+$plugin->release = '2.13';
 $plugin->maturity  = MATURITY_STABLE;
