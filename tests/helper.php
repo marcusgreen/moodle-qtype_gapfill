716c6c5555eab85359bbff5d7baa78c0519bce3a
Marcus Green <marcusavgreen@gmail.com>
2025-05-01 21:37:14 +0100

    Removed spurious internal check revealed by code checker

--- tests/helper.php
+++ tests/helper.php
@@ -21,7 +21,8 @@
  * @copyright  2013 Marcus Green
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-defined('MOODLE_INTERNAL') || die();
+global $CFG;
+require_once($CFG->dirroot . '/question/engine/tests/helpers.php');
 
 /**
  * utilities used by the other test classes
